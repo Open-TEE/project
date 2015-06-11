4c33ceb563f46df10ac947c1a8da3351f0cad88c
Ville Kankainen <villex.kankainen@intel.com>
2015-06-11 13:30:41 +0300

    updated android isntall script
    Change-Id: I0b49fa5f2ed075fe24873c69e04494577c798656
    Signed-off-by: Ville Kankainen <villex.kankainen@intel.com>

--- install_android.sh
+++ install_android.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 adb root
 adb remount
+
+adb shell mkdir /system/lib/ta
+adb shell mkdir /system/lib/tee
+
 adb push $OUT/system/lib/libtee.so /system/lib
 adb push $OUT/system/bin/conn_test_app /system/bin
 adb push $OUT/system/lib/libCommonApi.so /system/lib
@@ -9,4 +13,9 @@ adb push $OUT/system/lib/libLauncherApi.so /system/lib/tee
 adb push $OUT/system/lib/libManagerApi.so /system/lib/tee
 adb push $OUT/system/bin/opentee-engine /system/bin
 adb push $OUT/system/lib/libta_conn_test_app.so /system/lib/ta
+adb push $OUT/system/lib/libta_pkcs11.so /system/lib/ta
+adb push $OUT/system/lib/libta_storage_test.so /system/lib/ta
+adb push $OUT/system/bin/storage_test /system/bin
+adb push $OUT/system/bin/storage_test_ca /system/bin
+adb push $OUT/system/bin/pkcs11_test /system/bin
 adb push project/opentee.conf.android /etc/opentee.conf
